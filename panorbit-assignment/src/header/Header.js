4db6d09c10121877285a6582ec1af67317cef8f8
Ashutosh <ashutosh_14@outlook.com>
2023-08-20 13:53:23 +0530

    Modal

---
+++ panorbit-assignment/src/header/Header.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useState } from 'react'
+import './Header.css'
+import { Modal, Box, Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+const Header = (props) => {
+    const [open, setOpen] = useState(false);
+    const navigate = useNavigate()
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const onSignOut = () => {
+        setOpen(false)
+        navigate('/landing')
+    }
+    console.log(props)
+    return (
+        <>
+            <div className='header' onClick={handleOpen}>
+                <h3>Profile</h3>
+                <div className='header-profile'>
+                    <img className='profile-picture-header' src={props.props.profilepicture} alt='profile-pic' />
+                    <span>{props.props.name}</span>
+                </div>
+            </div>
+            <Modal className='modal'
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={{ position: "absolute", top: "10%", right: "18%", bgcolor: "background.paper", padding: "12px", borderRadius:"18px" }}>
+                    <div className='modal-content'>
+                        <img className='profile-picture-main' src={props.props.profilepicture} alt='profile-pic' />
+                        <p>{props.props.name}</p>
+                        <span className='label'>{props.props.email}</span>
+                    </div>
+                    <Button onClick={onSignOut} color="primary" sx={{
+                        borderRadius: 28, bgcolor: "#D55151", color: "white", '&:hover': {
+                            backgroundColor: 'black',
+                            color: 'white',
+                        },
+                    }}>Sign Out</Button>
+                </Box>
+            </Modal>
+        </>
+    )
+}
+
+export default Header
