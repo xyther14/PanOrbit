0f6cc54def879b53685a919d3a3e06d06be52198
Ashutosh <ashutosh_14@outlook.com>
2023-08-20 18:51:27 +0530

    Add chatbox

---
+++ panorbit-assignment/src/components/chatbox/ChatBox.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import './ChatBox.css'
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { List, ListItem, Divider } from '@mui/material';
+const ChatBox = (props) => {
+    console.log(props)
+    //remove current user from chat list
+    const otherUsers = props.props[0].users.filter(user => user.id !== props.props[1].id);
+    const [open, setOpen] = React.useState(false);
+    const openChat = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <div>
+            <div className="chat-bottom" onClick={openChat} style={{ bottom: open ? '50%' : '0' }}>
+                <div className='chat-bottom-icon'>
+                    <ChatBubbleOutlineIcon sx={{ marginRight: "12px" }} />
+                    <h4>Chats</h4>
+                </div>
+                <KeyboardArrowUpIcon sx={{display : !open ? 'block' : 'none'}}/>
+                <KeyboardArrowDownIcon sx={{display : !open ? 'none' : 'block'}}/>
+            </div>
+            <div className="chat-popup" style={{ display: open ? 'block' : 'none', bottom: open ? '40px': '0' }}>
+            <List style={{ maxHeight: '400px', overflow: 'auto' }}>
+            {otherUsers?.length > 0 && otherUsers.map((user) => {
+                    return <div className='list-item' key={user.id}>
+                        <ListItem>
+                            <div className='list-item-content'>
+                                <img className='profile-picture' src={user.profilepicture} alt='profile-pic' />
+                                {user.name}
+                            </div>
+                        </ListItem>
+                        <Divider />
+                    </div>
+                })}
+                </List>
+            </div>
+        </div>
+    );
+}
+
+export default ChatBox
--- panorbit-assignment/src/components/homePage/HomePage.js
+++ panorbit-assignment/src/components/homePage/HomePage.js
@@ -4,8 +4,9 @@ import { Outlet } from 'react-router-dom';
 import SideBar from '../sidebar/SideBar';
 import Header from '../../header/Header';
 import { useAPI } from '../../context/UserContext';
+import ChatBox from '../chatbox/ChatBox';
 const HomePage = (props) => {
-  const { authUser } = useAPI()
+  const { users, authUser } = useAPI()
   // let location = useLocation();
   // const user = location.state;
   return (
@@ -13,6 +14,7 @@ const HomePage = (props) => {
       <Header props={authUser}/>
       <SideBar/>
       <Outlet />
+      <ChatBox props={[users, authUser]}/>
     </div>
   )
 }
